011ad6c123c90b511fc64230df7222dabb6ec031
Matt Carvin <90224411+mcarvin8@users.noreply.github.com>
2026-09-11 11:12:22 -0400

    build(lint): lint unused exports and verify node engine against deps (#416)
    * build(lint): lint unused exports and verify node engine against deps
    
    * ci: change test trigger to PRs

---
+++ knip.config.ts
@@ -0,0 +1,15 @@
+export default {
+  entry: [
+    'src/commands/**/*.ts',
+    'src/action/*.ts',
+    'bin/dev.js',
+    'bin/run.js',
+    'scripts/**/*.{ts,mjs}',
+    '**/*.{nut,test,perf}.ts',
+    'vitest*.config.ts',
+    '.github/**/*.yml',
+  ],
+  project: ['**/*.{ts,js,mjs}'],
+  ignore: ['**/*.{json,yml,yaml}'],
+  ignoreExportsUsedInFile: true,
+};
